e147ba15ec83df95bac9d95de394fa55a5a9ccd1
shok1122 <shok1122@zohomail.com>
2025-08-10 01:23:20 +0000

    analyzer: Update endpoints and enhance debugging
    - Changed API endpoints to 192.168.220.5.
    - Added detailed debug logs for file input, argument checks, and template mismatches.
    - Introduced specific error codes (10–14) for clearer failure reasons.
    - Reordered dependency installation in Dockerfile for cleaner build.

--- docker/analyzer/main.py
+++ docker/analyzer/main.py
@@ -12,19 +12,26 @@ async def verify(response: Response, file: UploadFile = File(...)):
     if not file:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"Result": "NG", "Error": "file was not sent"}
+
+    print(f"Received file: {file.filename}, size: {file.size}")
     
     upload_dir = open(UPLOAD_PATH, "wb+")
     shutil.copyfileobj(file.file, upload_dir)
     upload_dir.close()
 
     with open(UPLOAD_PATH, "r") as f:
-        result, spec = analyzer.analyzer(f.read())
-    if (result == 1):
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"Result": "NG", "Error": "Incorrect format."}
-    else:
+        file_content = f.read()
+        print(f"File content length: {len(file_content)}")
+        print(f"First 100 chars: {file_content[:30]}")
+        result, spec = analyzer.analyzer(file_content)
+        print(f"Finish analyzer (code:{result})")
+
+    if (result == 0):
         response.status_code = status.HTTP_200_OK
         return {"Result": "OK", "ProcessingSpec": spec.__dict__}
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"Result": "NG", "Error": "Incorrect format."}
 
 @app.post("/update")
 async def update(response: Response, lib: bytes = File(...)):
