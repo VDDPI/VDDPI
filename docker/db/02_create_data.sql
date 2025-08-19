11a4549d58075905d7feb91d7fd116b5f915b42d
shok1122 <shok1122@zohomail.com>
2025-08-19 08:40:26 +0000

    Use MySQL variable for APP_ID in database initialization
    - Define @APP_ID variable to replace hardcoded values in policy inserts
    - Improve maintainability and consistency of test data setup

--- docker/db/02_create_data.sql
+++ docker/db/02_create_data.sql
@@ -1,16 +1,18 @@
+SET @APP_ID = '281d9646110a7a04dab0d6c01600c91ae73fe40bac7c57d';
+
 use provider;
 
 insert into policy(data_provider, data_type, data_id, data_consumer, data_processing, data_disclosing, data_counter, data_location, data_duration, data_expiration_date)
-values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-001", "consumer.example.com", "9049742836e22731ced39b84c0e7d473d007bc8e9815144", "person.age", 5, "JP", "90", "2026-12-01");
+values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-001", "consumer.example.com", @APP_ID, "person.age", 5, "JP", "90", "2026-12-01");
 
 insert into policy(data_provider, data_type, data_id, data_consumer, data_processing, data_disclosing, data_counter, data_location, data_duration, data_expiration_date)
-values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-002", "consumer.example.com", "9049742836e22731ced39b84c0e7d473d007bc8e9815144", "person.age", 5, "JP", "90", "2026-12-01");
+values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-002", "consumer.example.com", @APP_ID, "person.age", 5, "JP", "90", "2026-12-01");
 
 insert into policy(data_provider, data_type, data_id, data_consumer, data_processing, data_disclosing, data_counter, data_location, data_duration, data_expiration_date)
-values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-003", "consumer.example.com", "9049742836e22731ced39b84c0e7d473d007bc8e9815144", "person.age", 5, "JP", "90", "2026-12-01");
+values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-003", "consumer.example.com", @APP_ID, "person.age", 5, "JP", "90", "2026-12-01");
 
 insert into policy(data_provider, data_type, data_id, data_consumer, data_processing, data_disclosing, data_counter, data_location, data_duration, data_expiration_date)
-values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-004", "consumer.example.com", "9049742836e22731ced39b84c0e7d473d007bc8e9815144", "person.age", 5, "JP", "90", "2026-12-01");
+values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-004", "consumer.example.com", @APP_ID, "person.age", 5, "JP", "90", "2026-12-01");
 
 insert into policy(data_provider, data_type, data_id, data_consumer, data_processing, data_disclosing, data_counter, data_location, data_duration, data_expiration_date)
-values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-005", "consumer.example.com", "9049742836e22731ced39b84c0e7d473d007bc8e9815144", "person.age", 5, "JP", "90", "2026-12-01");
+values ("provider.example.com", "person", "https://192.168.220.7:443/data/person/personal-005", "consumer.example.com", @APP_ID, "person.age", 5, "JP", "90", "2026-12-01");
