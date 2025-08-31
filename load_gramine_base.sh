51709260a219c7fe157441e8ce5732c83ea1663f
shok1122 <shok1122@zohomail.com>
2025-08-31 08:34:28 +0000

    fix: correct hostname of registry from registry.vddpi to registry01.vddpi

--- load_gramine_base.sh
+++ load_gramine_base.sh
@@ -8,11 +8,11 @@ PATH_ID="./cache/$IMAGE_NAME.id"
 type gunzip >/dev/null 2>&1 || { echo "gunzip not found"; exit 1; }
 
 load_image() {
-	echo "Loading image from registry.vddpi"
-	ssh -T registry.vddpi "docker save $IMAGE_NAME:$IMAGE_LABEL | gzip -c" | gunzip | docker load
+	echo "Loading image from registry01.vddpi"
+	ssh -T registry01.vddpi "docker save $IMAGE_NAME:$IMAGE_LABEL | gzip -c" | gunzip | docker load
 }
 
-REMOTE_ID=$(ssh -T registry.vddpi "docker images --no-trunc --quiet $IMAGE_NAME:$IMAGE_LABEL 2>/dev/null")
+REMOTE_ID=$(ssh -T registry01.vddpi "docker images --no-trunc --quiet $IMAGE_NAME:$IMAGE_LABEL 2>/dev/null")
 LOCAL_ID=$(docker images --no-trunc --quiet $IMAGE_NAME:$IMAGE_LABEL 2>/dev/null)
 
 if [ -z "$REMOTE_ID" ]; then
