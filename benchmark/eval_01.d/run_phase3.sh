8833f9438f3ee867b231c27e681d3d8334b6350c
shok1122 <shok1122@zohomail.com>
2025-09-05 14:39:37 +0000

    Restart a container before scenario

--- benchmark/eval_01.d/run_phase3.sh
+++ benchmark/eval_01.d/run_phase3.sh
@@ -23,15 +23,16 @@ cache_dir="$3"
 # Main
 ########################################
 
+echo "Restart containers on consumer01.vddpi"
+(
+    cd $vddpi_dir
+    make stop-consumer > /dev/null 2>&1
+    MODE=eval-01 make run-consumer
+)
+
 # The first loop iteration retrieves data from the provider; the second iteration uses that data as cache.
 for scenario in "no_cache" "cache"
 do
-    echo "Restart containers on consumer01.vddpi"
-    (
-        cd $vddpi_dir
-        make stop-consumer > /dev/null 2>&1
-        MODE=eval-01 make run-consumer
-    )
 
     echo "Run gencert (scenario:$scenario)"
     python3 $vddpi_bench_dir/client.py gencert "$cache_dir"
