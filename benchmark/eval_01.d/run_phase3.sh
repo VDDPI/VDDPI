106d6b32a42e1b1ffa4afc49d7905acaa5e3fbef
shok1122 <shok1122@zohomail.com>
2025-09-05 09:45:33 +0000

    Measure processing time of client.py

--- benchmark/eval_01.d/run_phase3.sh
+++ benchmark/eval_01.d/run_phase3.sh
@@ -29,7 +29,14 @@ do
     for token in $cache_dir/token-*
     do
 
+        start_ts=$(date +"%Y-%m-%d %H:%M:%S")
+        start_epoch=$(date +%s%3N)
+
         msg=$(python3 ../client.py process "$token" "$cache_dir")
+
+        end_ts=$(date +"%Y-%m-%d %H:%M:%S")
+        end_epoch=$(date +%s%3N)
+        duration=$((end_epoch - start_epoch))
 
         echo "$msg" | head -n 1
         log=$(echo "$msg" | grep "Session completed" | sed -n 's/.*(\(.*\)).*/\1/p')
@@ -46,5 +53,6 @@ do
                 ;;
         esac
         echo "___BENCH___ Data processing ($log)" >> "$logfile"
+        echo "___BENCH___ Data processing (Start:$start_ts, End:$end_ts, Duration_ms:$duration, $log)" >> "result/tmp.log"
     done
 done
