6c020d2bf8d58eab930869b9d032fbeec8a6004d
shok1122 <shok1122@zohomail.com>
2025-09-05 23:01:39 +0900

    Enable visualization steps in run_visualization.sh
    - Uncommented execution of lib/xxx.py for app registration and data usage application graphs
    - Script now generates all visualization outputs instead of skipping initial ones

--- benchmark/eval_01.d/run_visualization.sh
+++ benchmark/eval_01.d/run_visualization.sh
@@ -1,15 +1,15 @@
 
 
-#python3 lib/xxx.py \
-#    result/graph_app_registration.svg \
-#    result/eval_app_registration.log \
-#    result/eval_obtaining_app_id.log \
-#    result/eval_code_analysis.log
-#
-#python3 lib/xxx.py \
-#    result/graph_data_usage_application.svg \
-#    result/eval_data_usage_application.log \
-#    result/eval_obtaining_processing_spec.log
+python3 lib/xxx.py \
+    result/graph_app_registration.svg \
+    result/eval_app_registration.log \
+    result/eval_obtaining_app_id.log \
+    result/eval_code_analysis.log
+
+python3 lib/xxx.py \
+    result/graph_data_usage_application.svg \
+    result/eval_data_usage_application.log \
+    result/eval_obtaining_processing_spec.log
 
 python3 lib/yyy.py \
     result/graph_data_processing.svg \
