c15d53c1b082e735233675dbba1ae676600976f4
shok1122 <shok1122@zohomail.com>
2025-09-13 07:28:38 +0000

    Ignore stderr

--- benchmark/record_stats.sh
+++ benchmark/record_stats.sh
@@ -27,7 +27,7 @@ echo "Timestamp,Container,MemUsage,CPU%,NetI/O,BlockI/O"
 while true; do
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
     # Get docker stats output once and add timestamp
-    docker stats --no-stream --format "table {{.Container}},{{.MemUsage}},{{.CPUPerc}},{{.NetIO}},{{.BlockIO}}" ${CONTAINERS[@]} | \
+    docker stats --no-stream --format "table {{.Container}},{{.MemUsage}},{{.CPUPerc}},{{.NetIO}},{{.BlockIO}}" ${CONTAINERS[@]} 2> /dev/null | \
     tail -n +2 | \
     while read line; do
         echo "$TIMESTAMP,$line"
