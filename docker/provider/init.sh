7b4dc73301752fb6c51b0d2378fa5fa2d3c78718
shok1122 <shok1122@zohomail.com>
2025-09-16 16:57:40 +0000

    For svm

--- docker/provider/init.sh
+++ docker/provider/init.sh
@@ -12,7 +12,13 @@ fi
 mode="$1"
 num_files="$2"
 
-rm -f provider/data/person/*
 if [ "$mode" = "eval-01" ]; then
+    rm -f ./data/person/*
     python3 ./make_test_data.py "$num_files" data/person 0;
+elif [ "$mode" = "eval-02" ]; then
+    rm -f ./data/svm/*
+    cp /svm_data/* ./data/svm
+else
+    echo "Unknown mode: $mode"
+    exit 1
 fi
