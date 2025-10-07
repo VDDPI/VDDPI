c7eb8a26e1744d623bef47f39ba88cf1f12c1ab9
shok1122 <shok1122@zohomail.com>
2025-10-07 11:16:37 +0000

    Updated

--- docker/provider/init.sh
+++ docker/provider/init.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <mode>"
-    echo ""
-    echo "mode:"
-    echo "  - demo" # To be implemented
-    echo "  - eval-01 <num_files>"
-    exit 1
-fi
-
 mode="$1"
-num_files="$2"
 
 if [ "$mode" = "eval-01" ]; then
+    num_files="$2"
     rm -f ./data/person/*
     python3 ./make_test_data.py "$num_files" data/person 0;
 elif [ "$mode" = "eval-02" ]; then
