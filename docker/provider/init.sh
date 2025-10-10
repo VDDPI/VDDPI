a1d855be57f001a157e52174a6b02f5ce378acc8
shok1122 <shok1122@zohomail.com>
2025-10-10 13:12:37 +0000

    Added eval-03

--- docker/provider/init.sh
+++ docker/provider/init.sh
@@ -9,6 +9,10 @@ if [ "$mode" = "eval-01" ]; then
 elif [ "$mode" = "eval-02" ]; then
     rm -f ./data/svm/*
     cp /svm_data/* ./data/svm
+elif [ "$mode" = "eval-03" ]; then
+    num_files="$2"
+    rm -f ./data/person/*
+    python3 ./make_test_data.py "$num_files" data/person 0;
 else
     echo "Unknown mode: $mode"
     exit 1
