f5ce2efccd0063ca745406056ed2c6a6f2d6a5ca
shok1122 <shok1122@zohomail.com>
2025-10-11 06:53:17 +0000

    Added todo comment

--- benchmark/eval_03.d/run_phase1.sh
+++ benchmark/eval_03.d/run_phase1.sh
@@ -44,4 +44,7 @@ app_id=$(jq -r '.DataProcessingSpec.App_ID' /tmp/response.json)
 echo "$app_id" > "$app_id_file"
 echo "App ID: $app_id"
 
+#TODO
+#ssh registry01.vddpi "docker exec registry-v1-gramine-1 cat /usr/bin/mypython" > mypython
+
 echo "___BENCH___ App registration (Start:$start_ts, End:$end_ts, Duration_ms:$duration)" >> "$LOGFILE"
